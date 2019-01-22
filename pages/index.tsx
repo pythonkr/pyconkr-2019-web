d9154755de4ad9fb6ef7167f4079c92c472d840c
jslee <jslee@solidware.io>
2019-01-22 19:26:11 +0900

    Add emotion/styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,31 @@
-export default () => <div>Welcome to PyconKR</div>
+
+import Link from 'next/link'
+import React from 'react'
+import { injectGlobal } from 'emotion'
+
+
+injectGlobal`
+  html, body {
+    padding: 3rem 1rem;
+    margin: 0;
+    background: papayawhip;
+    min-height: 100%;
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 24px;
+  }
+`
+
+export default class Index extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Link href='/about'>
+          <a>About</a>
+        </Link>
+        Index Page
+      </React.Fragment>
+    )
+  }
+}
+
+
