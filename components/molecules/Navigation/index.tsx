918fa9b93dbcd46c2ebd1fdd2d6333de9d4f3874
amazingguni <amazingguni@gmail.com>
2019-02-19 23:55:27 +0900

    Try graphql and login feature

--- components/molecules/Navigation/index.tsx
+++ components/molecules/Navigation/index.tsx
@@ -1,38 +1,41 @@
-import NavLink from 'components/atoms/NavLink'
-import styled from '@emotion/styled'
-import { contentWidth } from 'styles/layout'
+import NavLink from "components/atoms/NavLink";
+import styled from "@emotion/styled";
+import { contentWidth } from "styles/layout";
 
 const NavWrapper = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    max-width: ${contentWidth};
-`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  max-width: ${contentWidth};
+`;
 
 const NavItem = styled.li`
-    display: inline-block;
-    padding: 10px;
-`
+  display: inline-block;
+  padding: 10px;
+`;
 
 const Navigation = () => (
-    <NavWrapper>
-        <ul>
-            <NavItem>
-                <NavLink to='/sponsor' name='후원' />
-            </NavItem>
-            <NavItem>
-                <NavLink to='/contribute' name='공헌' />
-            </NavItem>
-        </ul>
-        <ul>
-            <NavItem>
-                <NavLink to='/account/login' name='로그인' />
-            </NavItem>
-            <NavItem>
-                <button onClick={() => {}}>English</button>
-            </NavItem>
-        </ul>
-    </NavWrapper>
-)
+  <NavWrapper>
+    <ul>
+      <NavItem>
+        <NavLink to="/sponsor" name="후원" />
+      </NavItem>
+      <NavItem>
+        <NavLink to="/contribute" name="공헌" />
+      </NavItem>
+    </ul>
+    <ul>
+      <NavItem>
+        <NavLink
+          to="https://github.com/login/oauth/authorize?client_id=e9bef37a5fda0f10f327&scope=user:email"
+          name="로그인"
+        />
+      </NavItem>
+      <NavItem>
+        <button onClick={() => {}}>English</button>
+      </NavItem>
+    </ul>
+  </NavWrapper>
+);
 
-export default Navigation
+export default Navigation;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,101 @@
-import React from 'react'
-import { inject, observer } from 'mobx-react'
-import { IRootStore } from 'stores/RootStore'
-import Footer from 'components/organisms/Footer'
-import Header from 'components/organisms/Header'
-import PageTemplate from 'components/templates/PageTemplate'
+import React from "react";
+import { inject, observer } from "mobx-react";
+import { IRootStore } from "stores/RootStore";
+import Footer from "components/organisms/Footer";
+import Header from "components/organisms/Header";
+import PageTemplate from "components/templates/PageTemplate";
+import qs from "qs";
+
+import gql from "graphql-tag";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 export type IndexPagePropsType = {
   stores: IRootStore;
-}
+};
 
-@inject('stores')
+@inject("stores")
 @observer
-class Index extends React.Component<{stores: IRootStore}> {
-    render () {
-      return (
-      <PageTemplate
-        header={<Header />}
-        footer={<Footer />}
-      >
+class Index extends React.Component<{ stores: IRootStore }> {
+  componentDidMount() {
+    if (location.search.indexOf("code") == -1) {
+      return;
+    }
+    const { code } = qs.parse(location.search, { ignoreQueryPrefix: true });
+    // 서버에 token 요청 하고
+    const httpLink = createHttpLink({
+      // uri: "http://dev.pycon.kr/api/graphql"
+      uri: "http://localhost:8000/api/graphql"
+    });
+
+    const authLink = setContext((_, { headers }) => {
+      // get the authentication token from local storage if it exists
+      const token = localStorage.getItem("token");
+      // return the headers to the context so httpLink can read them
+      return {
+        headers: {
+          ...headers,
+          authorization: token ? `JWT ${token}` : ""
+        }
+      };
+    });
+
+    const client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache()
+    });
+    client
+      .mutate({
+        variables: { oauthType: "github", code: code },
+        mutation: gql`
+          mutation OAuthTokenAuth($oauthType: String!, $code: String!) {
+            oAuthTokenAuth(oauthType: $oauthType, code: $code) {
+              token
+            }
+          }
+        `
+      })
+      .then(result => {
+        if (result.errors) {
+          throw new Error(`Authentication is failed: ${result.errors}`);
+        }
+
+        const token = result.data.oAuthTokenAuth.token;
+        console.log(token);
+        localStorage.setItem("token", token);
+        return client.query({
+          query: gql`
+            query {
+              profile {
+                username
+                email
+                profile {
+                  name
+                  avatarUrl
+                }
+              }
+            }
+          `
+        });
+      })
+      .then(response => {
+        console.log(response);
+        alert(JSON.stringify(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <PageTemplate header={<Header />} footer={<Footer />}>
         <span>Pycon HomePage</span>
       </PageTemplate>
-      )
-    }
+    );
+  }
 }
 
-export default Index
+export default Index;
