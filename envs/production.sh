2b05c6d92fbc5648dc4195b98110205888ff44e7
amazingguni <amazingguni@gmail.com>
2019-03-05 17:24:59 +0900

    Update facebook client id

--- envs/production.sh
+++ envs/production.sh
@@ -4,5 +4,5 @@ set -x
 export API_SERVER="https://www.pycon.kr/api/graphql"
 export GITHUB_CLIENT_ID="0dabbc90323dfc5753cc"
 export GOOGLE_CLIENT_ID=""
-export FACEBOOK_CLIENT_ID=""
+export FACEBOOK_CLIENT_ID="252822108953899"
 export NAVER_CLIENT_ID=""
