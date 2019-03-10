0379bce1c691d48aea870289f0fc783bec6c225b
amazingguni <amazingguni@gmail.com>
2019-03-10 11:34:21 +0900

    Add first og

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -15,6 +15,11 @@ class Header extends React.Component<PropsType> {
         <Head>
           <title>{title}</title>
           <meta charSet='utf-8' />
+          <meta property="og:site_name" content="PyCon Korea 2019" />
+          <meta property="og:title" content="PyCon Korea 2019" />
+          <meta property="og:url" content="https://pycon.kr" />
+          <meta property="og:description" content="파이콘 한국 2019: 8월 15일~18일(일), 코엑스 그랜드 볼룸" />
+          <meta property="og:image" content="/static/images/og-pyconkr-image.jpg" />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
         <Navigation />
