13733a1fe049a9e38a8732c78d3444a173c2ae92
amazingguni <amazingguni@gmail.com>
2019-03-10 16:41:07 +0900

    Support english version for og

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -1,6 +1,7 @@
 import Navigation from 'components/molecules/Navigation'
 import Head from 'next/head'
 import React from 'react'
+import intl from 'react-intl-universal'
 
 export type PropsType = {
   title: string;
@@ -15,10 +16,10 @@ class Header extends React.Component<PropsType> {
         <Head>
           <title>{title}</title>
           <meta charSet='utf-8' />
-          <meta property="og:site_name" content="PyCon Korea 2019" />
-          <meta property="og:title" content="PyCon Korea 2019" />
+          <meta property="og:site_name" content={intl.get("constant.pyconKorea.name").d("파이콘 한국 2019") />
+          <meta property="og:title" content={intl.get("constant.pyconKorea.name").d("파이콘 한국 2019") />
           <meta property="og:url" content="https://dev.pycon.kr/" />
-          <meta property="og:description" content="파이콘 한국 2019: 8월 15일~18일(일), 코엑스 그랜드 볼룸" />
+          <meta property="og:description" content={intl.get("constant.pyconKorea.scheduleWithNameAndVenue").d("파이콘 한국 2019: 8월 15일~18일(일), 코엑스 그랜드 볼룸")} />
           <meta property="og:image" content="/static/images/og-pyconkr-image.png" />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
