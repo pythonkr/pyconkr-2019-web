39b06c6266591fc7ff2319190c21932be761695b
Gyujin Cho <marina.gyujin.cho@gmail.com>
2019-03-10 18:06:27 +0900

    Add sponsor schedule table

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -16,11 +16,29 @@ class Header extends React.Component<PropsType> {
         <Head>
           <title>{title}</title>
           <meta charSet='utf-8' />
-          <meta property="og:site_name" content={intl.get("constant.pyconKorea.name").d("파이콘 한국 2019") />
-          <meta property="og:title" content={intl.get("constant.pyconKorea.name").d("파이콘 한국 2019") />
-          <meta property="og:url" content="https://dev.pycon.kr/" />
-          <meta property="og:description" content={intl.get("constant.pyconKorea.scheduleWithNameAndVenue").d("파이콘 한국 2019: 8월 15일~18일(일), 코엑스 그랜드 볼룸")} />
-          <meta property="og:image" content="/static/images/og-pyconkr-image.png" />
+          <meta
+            property='og:site_name'
+            content={
+              intl.get('constant.pyconKorea.name')
+                .d('파이콘 한국 2019')
+            }
+          />
+          <meta
+            property='og:title'
+            content={
+              intl.get('constant.pyconKorea.name')
+                .d('파이콘 한국 2019')
+            }
+          />
+          <meta property='og:url' content='https://dev.pycon.kr/'/>
+          <meta
+            property='og:description'
+            content={
+              intl.get('constant.pyconKorea.scheduleWithNameAndVenue')
+                .d('파이콘 한국 2019:  8월 15일~18일(일), 코엑스 그랜드 볼룸')
+            }
+          />
+          <meta property='og:  image' content='/static/images/og-pyconkr-image.png'/>
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
         <Navigation />
