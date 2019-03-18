25c61e3c9f7cc2312053886d349c76b7feb7efa4
amazingguni <amazingguni@gmail.com>
2019-03-18 13:57:49 +0900

    Fix production graphql url

--- envs/production.sh
+++ envs/production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export API_SERVER="http://www.pycon.kr/api/graphql"
+export API_SERVER="https://www.pycon.kr/api/graphql"
 export GITHUB_CLIENT_ID="0dabbc90323dfc5753cc"
 export GOOGLE_CLIENT_ID=""
 export FACEBOOK_CLIENT_ID="252822108953899"
