b0bb164674f9538f5414024e9031205e9ee93c62
aeyeong <iiogmgo@ridi.com>
2019-04-06 23:24:17 +0900

    lang 파라미터 없을시 accept-language 에 따라 언어 선택되도록

--- server.ts
+++ server.ts
@@ -14,6 +14,9 @@ app.prepare()
   .then(() => {
     createServer((req: any, res: any) => {
       const parsedUrl = parse(req.url, true)
+      if (!parsedUrl.query.lang) {
+        parsedUrl.query.lang = req.headers['accept-language'].split(',')[0]
+      }
       handle(req, res, parsedUrl)
     })
     .listen(port, (err: any) => {
