eda2aaeb3213718df38c02503d5dde923862e585
Gyujin Cho <gyujincho@lunit.io>
2019-05-02 23:49:15 +0900

    Add stage2 for review form

--- components/organisms/ProposalReviewForm/Stage2.tsx
+++ components/organisms/ProposalReviewForm/Stage2.tsx
@@ -1,27 +1,108 @@
+import { Button } from 'components/atoms/Button'
 import { FormWrapper } from 'components/atoms/ContentWrappers'
+import { FlexCenterWrapper } from 'components/atoms/FlexWrapper'
+import { IntlText } from 'components/atoms/IntlText'
+import { StageButtonGroup } from 'components/organisms/CFPForm/StageButtonGroup'
+import { LanguageNode } from 'lib/apollo_graphql/__generated__/globalTypes'
+import { Comment, ProposalReviewFormStage } from 'lib/stores/ProposalReview/ProposalReviewStore'
 import { inject, observer } from 'mobx-react'
 import { StoresType } from 'pages/_app'
 import React from 'react'
+import { TEAL } from 'styles/colors'
+import { toJS } from 'mobx';
 
 type State = {
-  // todo : implement here
+  comments: Comment[];
 }
 
 @inject('stores')
 @observer
 export default class Stage2 extends React.Component<{stores: StoresType; scrollRef: HTMLDivElement}, State> {
   state: State = {
-    // todo : implement here
+    comments: Array.from({ length: 7 }).map((_, index) => ({
+      proposalId: index,
+      text: ''
+    }))
   }
 
   async componentDidMount() {
-    // todo : implement here
+    const { stores } = this.props
+    const { proposalsToReview } = toJS(stores.proposalReviewStore)
+
+    this.props.scrollRef && window.scrollTo(0, this.props.scrollRef.offsetTop)
+    this.setState({
+      comments: proposalsToReview.map((proposal) => ({
+        proposalId: proposal.id,
+        text: ''
+      }))
+    })
+  }
+
+  isFormValid () {
+    return this.state.comments.length >= 7 &&
+      this.state.comments.every((comment) => comment.text.length > 20)
   }
 
   render () {
+    const { stores } = this.props
+    const { proposalsToReview } = toJS(stores.proposalReviewStore)
+
     return (
       <FormWrapper>
-        // todo : implement here
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          stores.proposalReviewStore.selectCategoriesAndOthers().then(() => {
+            stores.proposalReviewStore.setCurrentStage(ProposalReviewFormStage.stage2)
+          })
+        }}>
+          {proposalsToReview.map((proposal, index) => {
+            return <fieldset>
+              <legend className='required'>
+                <IntlText intlKey='contribute.talkProposal.application.stages.stages3.item1.sub1'>
+                  발표 제안 {index + 1}: {proposal.name}
+                </IntlText>
+              </legend>
+              <p style={{ marginBottom: 5 }}>
+                <IntlText intlKey='contribute.talkProposal.application.stages.stages3.item1.desc1'>
+                {proposal.category}
+                </IntlText> / <IntlText intlKey='contribute.talkProposal.application.stages.stages3.item1.desc1'>
+                  {proposal.difficulty}
+                </IntlText> / <IntlText   intlKey='contribute.talkProposal.application.stages.stages3.item1.desc1'>
+                  필요한 배경지식: {proposal.backgroundDesc}
+                </IntlText> / <IntlText intlKey='contribute.talkProposal.application.stages.stages3.item1.desc1'>
+                {proposal.duration}
+              </IntlText>
+              </p>
+              <p  style={{ marginBottom: 20 }}><IntlText intlKey='contribute.talkProposal.application.stages.stages3.item1.desc1'>
+                {proposal.detailDesc}
+              </IntlText></p>
+              <textarea
+                value={this.state.comments[index].text}
+                onChange={e => this.setState({
+                  comments: this.state.comments.map((comment) => ({
+                    ...comment,
+                    text: comment.proposalId === proposal.id ? e.target.value : comment.text
+                  }))
+                })}
+                aria-required={true}
+                style={{ height: 100, marginBottom: 10 }}
+                required
+              />
+            </fieldset>
+          })}
+          <FlexCenterWrapper style={{ marginTop: 80 }}>
+            <Button
+              type='submit'
+              tag='button'
+              intlKey='contribute.talkProposal.application.stages.stages1.button'
+              color={TEAL}
+              width={300}
+              disabled={!this.isFormValid()}
+            >
+              검토 의견 제출하기
+            </Button>
+          </FlexCenterWrapper>
+        </form>
       </FormWrapper>
     )
   }
--- lib/stores/ProposalReview/ProposalReviewStore.ts
+++ lib/stores/ProposalReview/ProposalReviewStore.ts
@@ -1,4 +1,5 @@
 import { action, configure, observable } from 'mobx'
+import { PresentationProposal } from '../CFP/PresentationProposal';
 
 configure({ enforceActions: 'observed' })
 
@@ -9,14 +10,32 @@ export enum ProposalReviewFormStage {
     completed = 3,
 }
 
+export type Comment = {
+    proposalId: number;
+    text: string;
+}
+
 export class ProposalReviewStore {
     // @observable isInitialized: boolean = false
     @observable currentStage: ProposalReviewFormStage = ProposalReviewFormStage.stage1
-    @observable proposalsToReview: object[] = []
+    @observable proposalsToReview: PresentationProposal[] = []
     @observable comments: object[] = []
 
     constructor() {
-        // this.proposal = new SponsorNode()
+        this.proposalsToReview = Array.from({ length: 7 })
+            .map((_, index) => ({
+                id: index + 3,
+                name: 'Why do I use Python for scientific research.',
+                detailDesc: `My research domain is hadrontherapy, which treat tumor with physical beams. One of the important research in this field is to calculate physical and biological dose fast and accurate to understand many effects on patients, such as Tumor Control Probability (TCP), Noraml Tissue Complication Probability (NTCP) and etc, and increase reproducible results respectively. Fast Recalculation on GPU (FRoG) is an in-house platform for Centro Nazionale Adrontherapia Oncologica (CNAO) in Italy and Heidelberg Ion Therapy center (HIT) in Germany. Recently, FRoG was installed at Aahrus proton center in Denmark and showed its potential to be used in other centers. FRoG is mainly programmed with python and CUDA to achieve fast calculation and flexibility together.
+
+                In the presentation, I will going to present about the two parts. One is about the advantage and the other is disadvantage of using python in scientific fields based on my experiences. The advantage of python is easy to learn, simple to code and well known libraries are existing. The disadvantage is more cultural. Mostly issued scientific areas are few statistical regression methods, such as neural network, and optimization which are called machine learning or Artificial Intelligent (AI).
+
+                Python is a powerful tool for science. However, the distance between scientists and python community seems not to be close. Presenting the differences in those two communities will help understanding each other.`,
+                difficulty: '초급',
+                category: '기타',
+                backgroundDesc: 'Scientific method',
+                duration: '45분'
+            }))
     }
 
     // @action
