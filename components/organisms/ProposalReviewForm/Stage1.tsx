443028d618b2764b1509005c9d6324fb6411fb5d
jslee <jslee@solidware.io>
2019-05-05 15:53:20 +0900

    오픈리뷰 언어 선택 기능 삭제

--- components/organisms/ProposalReviewForm/Stage1.tsx
+++ components/organisms/ProposalReviewForm/Stage1.tsx
@@ -2,7 +2,7 @@ import { Button } from 'components/atoms/Button'
 import { FormWrapper } from 'components/atoms/ContentWrappers'
 import { FlexCenterWrapper } from 'components/atoms/FlexWrapper'
 import { IntlText } from 'components/atoms/IntlText'
-import i18next from 'i18next';
+import i18next from 'i18next'
 import { LanguageNode } from 'lib/apollo_graphql/__generated__/globalTypes'
 import { ProposalReviewFormStage } from 'lib/stores/ProposalReview/ProposalReviewStore'
 import { observer } from 'mobx-react'
@@ -54,7 +54,7 @@ export default class Stage1 extends React.Component<{stores: StoresType; scrollR
   }
 
   render () {
-    const { stores } = this.props
+    const { stores, t } = this.props
     const { categoryIds } = this.state
 
     return (
@@ -67,9 +67,8 @@ export default class Stage1 extends React.Component<{stores: StoresType; scrollR
         }}>
           <fieldset>
             <legend className='required'>
-              <IntlText intlKey='contribute.talkProposal.application.stages.stages2.item3'>
-                세션 난이도
-              </IntlText>
+              {t('contribute:proposalReview.stages.stage1.selectCategoryTitle')}
+              <strong> ({categoryIds.length}/7)</strong>
             </legend>
             {
               categoryIds &&
@@ -89,55 +88,18 @@ export default class Stage1 extends React.Component<{stores: StoresType; scrollR
               )
             }
           </fieldset>
-          <fieldset>
-              <legend className='required'>
-                <IntlText intlKey='contribute.talkProposal.application.stages.stages2.item6.header'>
-                  언어
-                </IntlText>
-              </legend>
-              <p>
-                <input
-                  type='radio'
-                  id={LanguageNode.KOREAN}
-                  value={LanguageNode.KOREAN}
-                  aria-checked={this.state.language === LanguageNode.KOREAN}
-                  checked={this.state.language === LanguageNode.KOREAN}
-                  onChange={() => this.setState({ language: LanguageNode.KOREAN })}
-                />
-                <label htmlFor={LanguageNode.KOREAN}>
-                  <IntlText intlKey='contribute.talkProposal.application.stages.stages2.item6.sub1'>
-                    한국어
-                  </IntlText>
-                </label>
-              </p>
-              <p>
-                <input
-                  type='radio'
-                  id={LanguageNode.ENGLISH}
-                  value={LanguageNode.ENGLISH}
-                  aria-checked={this.state.language === LanguageNode.ENGLISH}
-                  checked={this.state.language === LanguageNode.ENGLISH}
-                  onChange={() => this.setState({ language: LanguageNode.ENGLISH })}
-                />
-                <label htmlFor={LanguageNode.ENGLISH}>
-                  <IntlText intlKey='contribute.talkProposal.application.stages.stages2.item6.sub2'>
-                    영어
-                  </IntlText>
-                </label>
-              </p>
-            </fieldset>
-            <FlexCenterWrapper style={{ marginTop: 80 }}>
-              <Button
-                type='submit'
-                tag='button'
-                intlKey='contribute.talkProposal.application.stages.stages1.button'
-                color={TEAL}
-                width={300}
-                disabled={!this.isFormValid()}
-              >
-                선택한 조건에 맞는 발표 제안 검토하기
-              </Button>
-          </FlexCenterWrapper>
+          <FlexCenterWrapper style={{ marginTop: 80 }}>
+            <Button
+              type='submit'
+              tag='button'
+              intlKey='contribute.talkProposal.application.stages.stages1.button'
+              color={TEAL}
+              width={300}
+              disabled={!this.isFormValid()}
+            >
+              선택한 조건에 맞는 발표 제안 검토하기
+            </Button>
+        </FlexCenterWrapper>
         </form>
       </FormWrapper>
     )
