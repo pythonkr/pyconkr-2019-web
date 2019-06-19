d1d1651e25b6afd6cd00e26f23730459ff861209
Lee Jae-yeol <rijgndqw012@gmail.com>
2019-06-19 21:20:35 +0900

    튜토리얼/스프린트 티켓 페이지에 설명 추가 (#172)
    * Add Banner Slider
    
    * SponsorBanners 컴포넌트를 PageTemplate에 적용
    
    * 후원등급 별 후원사 표시 기능 프로토타입 구현 완료
    
    * Banner 가운데 정렬
    
    * Banner 가운데 정렬
    
    * yarn lock 파일 에러 fix
    
    * 중복 선언 문제 해결
    
    * 티켓 페이지에 설명 삽입
    
    * 레거시 코드 수습
    
    * 빠트린 레거시 코드 제거
    
    * 디테일 페이지 임시로 URL 링크

--- pages/ticket/tutorial.tsx
+++ pages/ticket/tutorial.tsx
@@ -10,6 +10,8 @@ import { ticketMenu } from 'routes/paths'
 import { DateDTO } from 'types/common'
 import { StoresType } from '../_app'
 
+import { paths } from '../../routes/paths'
+
 export type IntlTextType = {
   intlKey: string;
   defaultText: string;
@@ -39,6 +41,16 @@ export default class ProposingATalk extends React.Component<{ stores: StoresType
         <H1><IntlText intlKey='ticket.tutorial.title'>
           튜토리얼 티켓
         </IntlText></H1>
+        <Paragraph><IntlText intlKey='ticket.tutorial.description1'>
+        튜토리얼은 초보자들을 위해, 또는 새로운 것을 접하는 사람들을 위해 진행하는 교육 프로그램입니다. 튜토리얼 진행자와 도움을 주는 사람들이 여러분을 기다리고 있습니다. 프로그래밍을 처음 시작하거나 파이썬을 새롭게 시작하는 분들에게 튜토리얼 프로그램을 추천합니다.
+        </IntlText></Paragraph>
+
+        <Paragraph><IntlText intlKey='ticket.tutorial.description2'>
+        기업에서 튜토리얼을 진행을 신청하는 경우, 파이콘 한국 준비위원회와 별도의 협의가 필요합니다. program@pycon.kr 로 연락주시면 안내해드리겠습니다.
+        </IntlText></Paragraph>
+
+        <H2>Tutorials</H2>
+        <Paragraph><a href={paths.ticket.tutorial.detail}>자세히 보기</a></Paragraph>
 
         <Section>
           <H2><IntlText intlKey='common.contact'>문의</IntlText></H2>
