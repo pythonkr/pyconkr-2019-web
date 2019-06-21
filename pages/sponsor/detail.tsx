7cedb5abd505c568aaa89e26208d1b280dd729f0
Hyeok Min Kwon <khmzxc@naver.com>
2019-06-21 21:46:30 +0900

    후원사 상세 페이지 수정

--- pages/sponsor/detail.tsx
+++ pages/sponsor/detail.tsx
@@ -24,7 +24,7 @@ const SponsorContent = (props) => {
   }
   return (<>
   <H1>{sponsor.name}</H1>
-  <H3 align="center">{sponsor.level.name}</H3>
+  <H3>{sponsor.level.name}</H3>
   <a href={sponsor.url} target="_blank">
     <SponsorLogo src={sponsor.logoImage} alt={sponsor.name}/>
   </a>
