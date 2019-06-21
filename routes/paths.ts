dbd9b515a5322bafb7be12a267e4842024dca276
amazingguni <amazingguni@gmail.com>
2019-06-21 18:30:54 +0900

    정적 페이지 추가추가추가

--- routes/paths.ts
+++ routes/paths.ts
@@ -9,8 +9,14 @@ export const paths = {
   },
   program: {
     base: '/program',
+    keynote: '/program/keynote',
     talks: '/program/talks',
-    talkDetail: '/program/talk-detail'
+    talkDetail: '/program/talk-detail',
+    lightningTalk: '/program/lightning-talk',
+    openSpaceTalk: '/program/open-space-talk',
+    tutorial: '/program/tutorial',
+    sprint: '/program/sprint',
+    youngcoder: '/program/youngcoder',
   },
   contribute: {
     base: '/contribute',
@@ -21,6 +27,7 @@ export const paths = {
     proposingASprint: '/contribute/proposing-a-sprint',
     proposingATutorial: '/contribute/proposing-a-tutorial',
     proposalReview: '/contribute/proposal-review',
+    volunteer: '/contribute/volunteer',
   },
   ticket: {
     base: '/ticket',
@@ -47,7 +54,7 @@ export const paths = {
     applicationForm: '/sponsor/application-form',
     termsOfService: '/sponsor/terms-of-service',
     patronList: '/sponsor/patrons',
-    detail: '/sponsor/detail'
+    detail: '/sponsor/detail',
   },
   account: {
     base: '/account',
@@ -117,9 +124,39 @@ export const programMenu: WithSubMenu = {
   basePath: paths.program.base,
   submenu: [
     {
+      title: '키노트',
+      intlKey: 'gnb.program.keynote',
+      link: paths.program.keynote,
+    },
+    {
       title: '발표 세션',
       intlKey: 'gnb.program.talks',
       link: paths.program.talks,
+    },
+    {
+      title: '라이트닝 토크',
+      intlKey: 'gnb.program.lightningTalk',
+      link: paths.program.lightningTalk,
+    },
+    {
+      title: '열린 공간',
+      intlKey: 'gnb.program.openSpaceTalk',
+      link: paths.program.openSpaceTalk,
+    },
+    {
+      title: '튜토리얼',
+      intlKey: 'gnb.program.tutorial',
+      link: paths.program.tutorial,
+    },
+    {
+      title: '스프린트',
+      intlKey: 'gnb.program.sprint',
+      link: paths.program.sprint,
+    },
+    {
+      title: '영코더',
+      intlKey: 'gnb.program.youngcoder',
+      link: paths.program.youngcoder,
     }
   ]
 }
@@ -158,6 +195,11 @@ export const contributionMenu: WithSubMenu = {
       title: '발표 제안하기',
       intlKey: 'gnb.contribute.proposingATalk',
       link: paths.contribute.proposingATalk,
+    },
+    {
+      title: '자원봉사',
+      intlKey: 'gnb.contribute.volunteer',
+      link: paths.contribute.volunteer,
     }
   ]
 }
