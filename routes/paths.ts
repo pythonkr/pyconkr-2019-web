2c390d7e3c4e12d9fb12bff3fe05af626771057b
Gyujin Cho <gyujincho@lunit.io>
2019-06-24 16:13:41 +0900

    Fix ticket submenu paths

--- routes/paths.ts
+++ routes/paths.ts
@@ -39,14 +39,8 @@ export const paths = {
     myTicket: '/ticket/my-ticket',
     receipt: '/ticket/receipt',
     conference: '/ticket/conference',
-    tutorial: {
-      base: '/ticket/tutorial',
-      detail: '/ticket/tutorialDetail' // TODO: 엔트리포인트 연결되면 :id 로 수정
-    },
-    sprint: {
-      base: '/ticket/sprint',
-      detail: '/ticket/sprintDetail' // TODO: 엔트리포인트 연결되면 :id 로 수정
-    },
+    tutorial: '/ticket/tutorial',
+    sprint: '/ticket/sprint',
     youngcoder: '/ticket/youngcoder',
     childcare: '/ticket/childcare',
     payment: '/ticket/payment'
