daeb9b913334cdc622ac74d4795603d42d84e092
amazingguni <amazingguni@gmail.com>
2019-06-26 22:42:40 +0900

    튜토리얼 리스트 및 상세 페이지 추가 (#189)
    * 튜토리얼 목록 추가
    
    * Remove log

--- pages/program/tutorial.tsx
+++ pages/program/tutorial.tsx
@@ -1,4 +1,4 @@
-import { H1, H2, Paragraph, Section } from 'components/atoms/ContentWrappers'
+import { H1, H2, Ul, Li, Paragraph, Section } from 'components/atoms/ContentWrappers'
 import { LocalNavigation } from 'components/molecules/LocalNavigation'
 import Footer from 'components/organisms/Footer'
 import Header from 'components/organisms/Header'
@@ -7,9 +7,44 @@ import i18next from 'i18next'
 import React from 'react'
 import { programMenu } from 'routes/paths'
 import { withNamespaces } from '../../i18n'
+import {Loading} from 'components/atoms/Loading'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+const TUTORIALS = gql`
+query Tutorials {
+  tutorials {
+    id
+    name
+    owner {
+      profile {
+        name
+      }
+    }
+    difficulty {
+      name
+    }
+  }
+}
+`
 
 export type PropsType = {
   t: i18next.TFunction;
+}
+
+const TutorialList = (props) => {
+  const { tutorials }  = props
+  return (<>
+    <Ul>
+      {
+        tutorials.map(({id, name, owner, difficulty}) => {
+          return (
+            <Li key={id}>{name}/{owner ? owner.profile.name : ''}/{difficulty.name}</Li>
+          )
+        })
+      }
+    </Ul>
+  </>)
 }
 
 export class Tutorial extends React.Component<PropsType> {
@@ -28,11 +63,23 @@ export class Tutorial extends React.Component<PropsType> {
         </H1>
         <Section>
           <Paragraph>
-            { t('program:tutorial.desc1') }
+            { t('program:tutorial.desc1-1') }
           </Paragraph>
           <Paragraph>
-            { t('program:tutorial.desc2') }
+            { t('program:tutorial.desc1-2') }
           </Paragraph>
+        </Section>
+        <Section>
+          <H2>{ t('program:tutorial.header2') }</H2>
+          <Paragraph>{ t('program:tutorial.desc2') }</Paragraph>
+          <Query query={TUTORIALS}>
+            {({ loading, error, data }) => {
+              if (loading || error) return (<Loading width={50} height={50}/>);
+              return (
+                <TutorialList tutorials={ data.tutorials }/>
+              )
+            }}
+          </Query>
         </Section>
         <Section>
           <H2>{t('common:contact')}</H2>
