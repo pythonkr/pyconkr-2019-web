35734ec708093ab5cb8a80a318baf0e00e16ec16
lexifdev <sl@lxf.kr>
2020-07-03 06:36:20 +0900

    API_SERVER 환경변수가 제공되면 환경변수를 쓰도록

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -x
 
+if ! [ -z "API_SERVER" ]; then
+    export PROVIDED_API_SERVER=$API_SERVER
+fi
+
 if [ -z "$DEPLOY_ENV" ]; then
     source ./envs/development.sh
 else
     source ./envs/${DEPLOY_ENV}.sh
 fi
+
+if ! [ -z "PROVIDED_API_SERVER" ]; then
+    export API_SERVER=$PROVIDED_API_SERVER
+fi
+
 yarn && yarn build
 yarn start
 
